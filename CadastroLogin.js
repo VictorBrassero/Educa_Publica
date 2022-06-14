04c52a54159343d320d19b54af1b328af8cc8364
IgorGiovani <88683606+IgorGiovani@users.noreply.github.com>
2022-06-14 15:20:32 -0300

    JavaScript da pagina de cadastro e de login

---
+++ CadastroLogin.js
@@ -0,0 +1,40 @@
+var btnSignin = document.querySelector("#signin");
+var btnSignup = document.querySelector("#signup");
+
+var body = document.querySelector("body");
+
+
+btnSignin.addEventListener("click", function () {
+   body.className = "sign-in-js"; 
+});
+
+btnSignup.addEventListener("click", function () {
+    body.className = "sign-up-js";
+})
+
+//Função para verificação de preenchimento de campos do formulário
+
+function verificar(){
+    var nome = document.getElementById('Nome').value;
+    var email = document.getElementById('Email').value;
+    var senha = document.getElementById('Senha').value;
+    
+
+     if(!nome || !email || !senha){
+        alert("Campos não preenchidos, favor preencê-los");
+    } else{
+        window.location.href="Menu.html";    
+    }
+}
+
+function verificar2(){
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
+    
+
+     if(!email || !senha){
+        alert("Campos não preenchidos, favor preencê-los");
+    } else{
+        window.location.href="Menu.html";    
+    }
+}
